ed939be20a5b97b6b07c8146a4f2bcd310b26605
Taha Babar <tahababar70@gmail.com>
2020-08-27 19:33:36 +0500

    Add files via upload

---
+++ Cat Or Dog/Cat Or Dog/ViewController.swift
@@ -0,0 +1,73 @@
+//
+//  ViewController.swift
+//  Cat Or Dog
+//
+//  Created by Taha Babar on 8/23/20.
+//  Copyright © 2020 Taha Babar. All rights reserved.
+//
+
+import UIKit
+import Vision
+import CoreML
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet weak var answer: UIBarButtonItem!
+    @IBOutlet weak var viewResult: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imagePicker.delegate = self
+    }
+
+    @IBAction func cameraButton(_ sender: UIBarButtonItem) {
+        imagePicker.sourceType = .photoLibrary //to allow camera functionality //do .camera if u want to access photos from photo library
+                      imagePicker.allowsEditing = false //to disable editing on pictures by users
+                      present(imagePicker, animated: true, completion: nil)  //to open camera
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let image = info[.originalImage] as? UIImage { //do .editedImage if allowsediting is true
+            viewResult.image = image       //assigning that image to our image view
+            imagePicker.dismiss(animated: true, completion: nil)
+            //to get rid of imagePicker/album/camera after u select/capture a photo
+
+            guard let ciImage = CIImage(image: image) else {
+               fatalError("couldn't convert uiimage to CIImage")
+            }
+            detect(image: ciImage)
+        }
+    }
+    
+    func detect(image: CIImage) {
+        
+        // Load the ML model through its generated class //creating a new model for image analysis
+        guard let model = try? VNCoreMLModel(for: CatOrDog().model) else {
+            fatalError("can't load ML model")
+        }
+        //creating request of detector
+        let request = VNCoreMLRequest(model: model) { request, error in
+            guard let results = request.results as? [VNClassificationObservation],
+                let topResult = results.first
+                else {
+                    fatalError("unexpected result type from VNCoreMLRequest")
+            }
+                DispatchQueue.main.async {
+                    print(topResult.identifier);
+                 self.answer.title = "Result: \(topResult.identifier)".capitalized; //to capitalize our string
+                }
+        }
+        //to link image woith detector to get final results
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try handler.perform([request])
+        }
+        catch {
+            print(error)
+        }
+    }
+}
+
